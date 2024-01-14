9a2e834983546682c3efb06a1271251da2612ec8
ming xiong <iona.m.xiong@gmail.com>
2024-01-14 16:01:36 +0200

    test the master branch protection rule

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ server.use(middlewares);
 
 server.use(router);
 
+server.get("/api/anecdotes/:id", (req, res) => {
+  const id = req.params.id;
+  const anecdote = router.db
+    .get("anecdotes")
+    .find({ id: id })
+    .value();
+  res.send(anecdote);
+});
+
 server.listen(process.env.PORT || 3001, () => {
   console.log("JSON Server is running");
 });
