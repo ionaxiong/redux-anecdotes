ec56490d1b1749f69cf1f44bb9c8002575ca2764
ming xiong <iona.m.xiong@gmail.com>
2024-01-14 15:01:43 +0200

    added pipeline workflow

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults({
   static: "./dist",
@@ -10,7 +10,9 @@ const middlewares = jsonServer.defaults({
 
 server.use(middlewares);
 
-server.use("/", router);
+server.use("/health", (req, res) => {
+  res.send("OK");
+});
 
 server.listen(process.env.PORT || 3001, () => {
   console.log("JSON Server is running");
